99a89843c7cc5c2557e58faee02e367ee492b2ac
yeji <skql4128@naver.com>
2024-10-02 18:05:50 +0900

    [feat] 식당 정보 UI 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,22 @@ import Map from '@/components/Map';
 import Markers from '@/components/Markers';
 
 import * as stores from '@/data/store_data.json';
+import StoreBox from '@/components/StoreBox';
 
 export default function Home() {
   const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
   const storeDatas = stores['DATA'];
 
   return (
     <>
       <Map setMap={setMap} />
-      <Markers storeDatas={storeDatas} map={map} />
+      <Markers
+        storeDatas={storeDatas}
+        map={map}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
 }
