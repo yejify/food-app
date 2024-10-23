090e2ea8c7568d88ecc20f91ff96cebef019a34f
yeji <skql4128@naver.com>
2024-10-24 01:25:22 +0900

    [fix] data fetching 방식 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,10 @@ export default function Home({ stores }: { stores: StoreType[] }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
 
   return {
     props: { stores: stores.data },
-    revalidate: 60 * 60,
   };
 }
