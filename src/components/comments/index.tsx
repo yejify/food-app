c72e4b404bce2338b326cba9df6a2b11e1220c61
yeji <skql4128@naver.com>
2024-10-29 00:09:06 +0900

    [fix] 댓글 페이지네이션 수정

--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -1,4 +1,5 @@
-/* eslint-disable @next/next/no-img-element */
+'use client';
+
 import { useSession } from 'next-auth/react';
 import CommentForm from './CommentForm';
 import { useQuery } from '@tanstack/react-query';
@@ -6,17 +7,16 @@ import axios from 'axios';
 import { CommentApiResponse } from '@/interface';
 import CommentList from './CommentList';
 import Pagination from '../Pagination';
+import { useSearchParams } from 'next/navigation'; // 쿼리 파라미터 처리
 
 interface CommentProps {
   storeId: number;
-  params?: {
-    page?: string;
-  };
 }
 
-export default function Comments({ storeId, params }: CommentProps) {
+export default function Comments({ storeId }: CommentProps) {
   const { status } = useSession();
-  const page = params?.page || '1';
+  const searchParams = useSearchParams(); // 쿼리 파라미터 가져오기
+  const page = searchParams?.get('page') || '1'; // 쿼리 파라미터에서 페이지 가져오기
 
   const fetchComments = async () => {
     const { data } = await axios(
@@ -26,25 +26,33 @@ export default function Comments({ storeId, params }: CommentProps) {
     return data as CommentApiResponse;
   };
 
-  const { data: comments, refetch } = useQuery({
-    queryKey: [`comments-${storeId}-${page}`],
+  const {
+    data: comments,
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ['comments', storeId, page],
     queryFn: fetchComments,
   });
 
   return (
     <div className='md:max-w-2xl py-8 px-2 mb-20 mx-auto'>
-      {/* comment form */}
       {status === 'authenticated' && (
         <CommentForm storeId={storeId} refetch={refetch} />
       )}
-      {/* comment list */}
-      <CommentList comments={comments} />
-      {/* pagination */}
-      <Pagination
-        total={comments?.totalPage}
-        page={page}
-        pathname={`/stores/${storeId}`}
-      />
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <CommentList comments={comments} />
+          <Pagination
+            total={comments?.totalPage || 1}
+            page={page}
+            pathname={`/stores/${storeId}`}
+          />
+        </>
+      )}
     </div>
   );
 }
