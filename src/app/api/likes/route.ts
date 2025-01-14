8dfdc5732eadceb3ea5a06a43ba10664fa13393a
yeji <skql4128@naver.com>
2025-01-14 23:45:21 +0900

    [fix] like GET, POST api 수정 #3

--- src/app/api/likes/route.ts
+++ src/app/api/likes/route.ts
@@ -3,7 +3,6 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import prisma from '@/db';
 
-// POST 요청: 댓글 생성
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
 
@@ -11,91 +10,72 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
-  const { storeId, body }: { storeId: number; body: string } = await req.json();
+  const { storeId }: { storeId: number } = await req.json();
 
   try {
-    const comment = await prisma.comment.create({
-      data: {
+    let like = await prisma.like.findFirst({
+      where: {
         storeId,
-        body,
         userId: session.user.id,
       },
     });
 
-    return NextResponse.json(comment, { status: 201 });
+    if (like) {
+      await prisma.like.delete({ where: { id: like.id } });
+      // 상태 코드 204로 응답
+      return new NextResponse(null, { status: 204 }); // 빈 본문과 함께 204 반환
+    } else {
+      like = await prisma.like.create({
+        data: { storeId, userId: session.user.id },
+      });
+      return NextResponse.json(like, { status: 201 }); // 201 상태 코드로 응답
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: 'Failed to create comment' },
+      { error: 'Failed to handle like' },
       { status: 500 }
     );
   }
 }
 
-// DELETE 요청: 댓글 삭제
-export async function DELETE(req: Request) {
-  const session = await getServerSession(authOptions);
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get('id');
-
-  if (!session?.user || !id) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
-
-  try {
-    const result = await prisma.comment.delete({
-      where: { id: parseInt(id) },
-    });
-
-    return NextResponse.json(result, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: 'Failed to delete comment' },
-      { status: 500 }
-    );
-  }
-}
-
-// GET 요청: 댓글 목록 조회
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const page = searchParams.get('page') || '1';
   const limit = searchParams.get('limit') || '10';
-  const storeId = searchParams.get('storeId');
-  const user = searchParams.get('user') === 'true';
-
-  const skipPage = parseInt(page) - 1;
+  const skipPage = (parseInt(page) - 1) * parseInt(limit);
   const session = await getServerSession(authOptions);
 
+  if (!session?.user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
   try {
-    const count = await prisma.comment.count({
+    const count = await prisma.like.count({
       where: {
-        storeId: storeId ? parseInt(storeId) : undefined,
-        userId: user ? session?.user?.id : undefined,
+        userId: session.user.id,
       },
     });
 
-    const comments = await prisma.comment.findMany({
-      orderBy: { createdAt: 'desc' },
+    // 해당 유저가 좋아요를 누른 가게들을 가져옴
+    const likes = await prisma.like.findMany({
       where: {
-        storeId: storeId ? parseInt(storeId) : undefined,
-        userId: user ? session?.user?.id : undefined,
+        userId: session.user.id,
       },
-      skip: skipPage * parseInt(limit),
+      skip: skipPage,
       take: parseInt(limit),
-      include: { user: true, store: true },
+      include: { store: true },
     });
 
     return NextResponse.json({
-      data: comments,
+      data: likes,
       page: parseInt(page),
       totalPage: Math.ceil(count / parseInt(limit)),
     });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: 'Failed to fetch comments' },
+      { error: 'Failed to fetch likes' },
       { status: 500 }
     );
   }
